5cbe386ac8d10f1e9c87d6b2d7a8d86bf7783090
Gabriel_P <108835349+Gabriel-P-Freitas@users.noreply.github.com>
2023-10-03 01:30:34 +0000

    Pending changes exported from your codespace

--- diario.py
+++ diario.py
@@ -5,9 +5,13 @@ class Diario(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     titulo = db.Column(db.String(100))
     disciplina = db.Column(db.String(100))
+    id_professor = db.Column(db.Integer, db.ForeignKey('professor.id'))
 
-    def __init__(self, titulo, disciplina):
+    professor = db.relationship('Professor', foreign_keys = id_professor) # Cria a relação com a classe Professor de acordo com seu ID
+
+    def __init__(self, titulo, disciplina, id_professor):
         self.titulo = titulo
         self.disciplina = disciplina
+        self.id_professor = id_professor # Referencia a coluna estrangeira
 
         # Os dados dessa classe serão usados no banco de dados para o armazenamento nas tabelas, colunas etc.
